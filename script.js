49025c86b0dda4f5653cbfdb5d16637d3dd1725e
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-19 23:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+    search.classList.toggle("active"); // Toggle active class
+    input.focus(); // Focus on input field
+});
